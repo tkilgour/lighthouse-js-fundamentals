68d6400a643ba527d47dff2adfaaa6f9ca127402
Thomas Kilgour <t.kilgour@gmail.com>
2017-01-09 19:03:22 -0500

    refactored loopyLighthouse

--- loopy-lighthouse2.js
+++ loopy-lighthouse2.js
@@ -1,14 +1,14 @@
 function loopyLighthouse(range, multiples, words) {
-  for (var i = range[0]; i <= range[1]; i++) {
-    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
-      console.log(words[0] + words[1]);
-    } else if (i % multiples[0] === 0) {
-      console.log(words[0]);
-    } else if (i % multiples[1] === 0) {
-      console.log(words[1]);
-    } else {
-      console.log(i);
+  for (var num = range[0]; num <= range[1]; num++) {
+    var output = "";
+
+    if (num % multiples[0] === 0) {
+      output += words[0];
     }
+    if (num % multiples[1] === 0) {
+      output += words[1];
+    }
+    console.log(output === "" ? num : output);
   }
 }
 
